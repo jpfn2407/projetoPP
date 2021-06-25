35272db4bb62c14f40703bda8d49f539dc44dd42
João Ferreira <jpfn2407@gmail.com>
2021-06-26 00:47:14 +0100

    Comentado e organizado. Falta criar a zona

--- src/main/java/pt/ual/pp/projeto/models/Car.java
+++ src/main/java/pt/ual/pp/projeto/models/Car.java
@@ -1,4 +1,35 @@
 package pt.ual.pp.projeto.models;
 
+import pt.ual.pp.projeto.models.sequence.ModelSequence;
+import pt.ual.pp.projeto.models.sequence.SequenceInfo;
+
 public class Car {
+    private String modelID;
+    private double buildTime = 0.0;
+    private ModelSequence modelSequence;
+
+    public Car(String modelID, ModelSequence modelSequence) {
+        this.modelID = modelID;
+        this.modelSequence = modelSequence;
+    }
+
+    public String getModelID() {
+        return modelID;
+    }
+
+    public boolean isFinished(){
+        return this.modelSequence.isFinished();
+    }
+
+    public SequenceInfo getNextNotDone(){
+        return this.modelSequence.getNextNotDone();
+    }
+
+    public double getBuildTime() {
+        return buildTime;
+    }
+
+    public void setBuildTime(double buildTime){
+        this.buildTime = buildTime;
+    }
 }
