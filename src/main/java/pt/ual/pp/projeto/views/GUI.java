f569f98d4570993870d176c9601e61f652b54a4d
hugo-fsd <62383769+hugo-fsd@users.noreply.github.com>
2021-06-27 11:19:57 +0100

    only missing progress bar takes the same time as the simulation

--- src/main/java/pt/ual/pp/projeto/views/GUI.java
+++ src/main/java/pt/ual/pp/projeto/views/GUI.java
@@ -1,15 +1,15 @@
 package pt.ual.pp.projeto.views;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ProgressBar;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import pt.ual.pp.projeto.controllers.Controller;
@@ -50,39 +50,40 @@ public class GUI extends Application implements Initializable {
     public TextField model1Zone3AverageTime;
     public TextField model1Zone4AverageTime;
     public TextField model1Zone5AverageTime;
-    public TextField model1Zone1Order;
-    public TextField model1Zone2Order;
-    public TextField model1Zone3Order;
-    public TextField model1Zone4Order;
-    public TextField model1Zone5Order;
+    public ComboBox model1Zone1Order;
+    public ComboBox model1Zone2Order;
+    public ComboBox model1Zone3Order;
+    public ComboBox model1Zone4Order;
+    public ComboBox model1Zone5Order;
 
     public TextField model2Zone1AverageTime;
     public TextField model2Zone2AverageTime;
     public TextField model2Zone3AverageTime;
     public TextField model2Zone4AverageTime;
     public TextField model2Zone5AverageTime;
-    public TextField model2Zone1Order;
-    public TextField model2Zone2Order;
-    public TextField model2Zone3Order;
-    public TextField model2Zone4Order;
-    public TextField model2Zone5Order;
+    public ComboBox model2Zone1Order;
+    public ComboBox model2Zone2Order;
+    public ComboBox model2Zone3Order;
+    public ComboBox model2Zone4Order;
+    public ComboBox model2Zone5Order;
 
     public TextField model3Zone1AverageTime;
     public TextField model3Zone2AverageTime;
     public TextField model3Zone3AverageTime;
     public TextField model3Zone4AverageTime;
     public TextField model3Zone5AverageTime;
-    public TextField model3Zone1Order;
-    public TextField model3Zone2Order;
-    public TextField model3Zone3Order;
-    public TextField model3Zone4Order;
-    public TextField model3Zone5Order;
+    public ComboBox model3Zone1Order;
+    public ComboBox model3Zone2Order;
+    public ComboBox model3Zone3Order;
+    public ComboBox model3Zone4Order;
+    public ComboBox model3Zone5Order;
 
     public TextArea averageBuildTimeOutput;
     public TextArea averageWaitTimeOutput;
     public TextArea averageUsagePercentageOutput;
 
     public CheckBox erlangToggle;
+
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -92,6 +93,7 @@ public class GUI extends Application implements Initializable {
         Parent root = fxmlLoader.load();
         primaryStage.setTitle("Projecto Paradigmas Programação");
         primaryStage.setScene(new Scene(root, 1000, 800));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
@@ -122,25 +124,25 @@ public class GUI extends Application implements Initializable {
 
         //Choose Model Order and Average time by Zone----------------------------------------------------------------------------------------------------------------
         //Model 1
-        controller.addSequenceInfo("1", String.valueOf(model1Zone1Order.getText()), "1", String.valueOf(model1Zone1AverageTime.getText()));
-        controller.addSequenceInfo("1", String.valueOf(model1Zone2Order.getText()), "2", String.valueOf(model1Zone2AverageTime.getText()));
-        controller.addSequenceInfo("1", String.valueOf(model1Zone3Order.getText()), "3", String.valueOf(model1Zone3AverageTime.getText()));
-        controller.addSequenceInfo("1", String.valueOf(model1Zone4Order.getText()), "4", String.valueOf(model1Zone4AverageTime.getText()));
-        controller.addSequenceInfo("1", String.valueOf(model1Zone5Order.getText()), "5", String.valueOf(model1Zone5AverageTime.getText()));
+        controller.addSequenceInfo("1", String.valueOf(model1Zone1Order.getValue()), "1", String.valueOf(model1Zone1AverageTime.getText()));
+        controller.addSequenceInfo("1", String.valueOf(model1Zone2Order.getValue()), "2", String.valueOf(model1Zone2AverageTime.getText()));
+        controller.addSequenceInfo("1", String.valueOf(model1Zone3Order.getValue()), "3", String.valueOf(model1Zone3AverageTime.getText()));
+        controller.addSequenceInfo("1", String.valueOf(model1Zone4Order.getValue()), "4", String.valueOf(model1Zone4AverageTime.getText()));
+        controller.addSequenceInfo("1", String.valueOf(model1Zone5Order.getValue()), "5", String.valueOf(model1Zone5AverageTime.getText()));
 
         //Model 2
-        controller.addSequenceInfo("2", String.valueOf(model2Zone1Order.getText()), "1", String.valueOf(model2Zone1AverageTime.getText()));
-        controller.addSequenceInfo("2", String.valueOf(model2Zone2Order.getText()), "2", String.valueOf(model2Zone2AverageTime.getText()));
-        controller.addSequenceInfo("2", String.valueOf(model2Zone3Order.getText()), "3", String.valueOf(model2Zone3AverageTime.getText()));
-        controller.addSequenceInfo("2", String.valueOf(model2Zone4Order.getText()), "4", String.valueOf(model2Zone4AverageTime.getText()));
-        controller.addSequenceInfo("2", String.valueOf(model2Zone5Order.getText()), "5", String.valueOf(model2Zone5AverageTime.getText()));
+        controller.addSequenceInfo("2", String.valueOf(model2Zone1Order.getValue()), "1", String.valueOf(model2Zone1AverageTime.getText()));
+        controller.addSequenceInfo("2", String.valueOf(model2Zone2Order.getValue()), "2", String.valueOf(model2Zone2AverageTime.getText()));
+        controller.addSequenceInfo("2", String.valueOf(model2Zone3Order.getValue()), "3", String.valueOf(model2Zone3AverageTime.getText()));
+        controller.addSequenceInfo("2", String.valueOf(model2Zone4Order.getValue()), "4", String.valueOf(model2Zone4AverageTime.getText()));
+        controller.addSequenceInfo("2", String.valueOf(model2Zone5Order.getValue()), "5", String.valueOf(model2Zone5AverageTime.getText()));
 
         //Model 3
-        controller.addSequenceInfo("3", String.valueOf(model3Zone1Order.getText()), "1", String.valueOf(model3Zone1AverageTime.getText()));
-        controller.addSequenceInfo("3", String.valueOf(model3Zone2Order.getText()), "2", String.valueOf(model3Zone2AverageTime.getText()));
-        controller.addSequenceInfo("3", String.valueOf(model3Zone3Order.getText()), "3", String.valueOf(model3Zone3AverageTime.getText()));
-        controller.addSequenceInfo("3", String.valueOf(model3Zone4Order.getText()), "4", String.valueOf(model3Zone4AverageTime.getText()));
-        controller.addSequenceInfo("3", String.valueOf(model3Zone5Order.getText()), "5", String.valueOf(model3Zone5AverageTime.getText()));
+        controller.addSequenceInfo("3", String.valueOf(model3Zone1Order.getValue()), "1", String.valueOf(model3Zone1AverageTime.getText()));
+        controller.addSequenceInfo("3", String.valueOf(model3Zone2Order.getValue()), "2", String.valueOf(model3Zone2AverageTime.getText()));
+        controller.addSequenceInfo("3", String.valueOf(model3Zone3Order.getValue()), "3", String.valueOf(model3Zone3AverageTime.getText()));
+        controller.addSequenceInfo("3", String.valueOf(model3Zone4Order.getValue()), "4", String.valueOf(model3Zone4AverageTime.getText()));
+        controller.addSequenceInfo("3", String.valueOf(model3Zone5Order.getValue()), "5", String.valueOf(model3Zone5AverageTime.getText()));
         //-----------------------------------------------------------------------------------------------------------------------------------------------------------
 
         //Choose number of lines per zone----------------------------------------------------------------------------------------------------------------------------
@@ -154,30 +156,30 @@ public class GUI extends Application implements Initializable {
         //Enable Erlang----------------------------------------------------------------------------------------------------------------------------------------------
         controller.setUseErlang(!erlangToggle.isSelected());
 
-        executorService.submit(()->{
-            controller.startSimulation();
+        executorService.submit(() -> {
+            double time = Double.parseDouble(tempString);
+            double counter = 0.0;
+            while (counter <= 1) {
+                progressBar.progressProperty().setValue(counter);
+                try {
+                    TimeUnit.MICROSECONDS.sleep(Math.round(0.0001 * time * 1_000_000));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                counter += 100/time;
+            }
         });
 
         //Start simulation
         executorService.submit(() -> {
-            double counter = 0.0;
-            while(counter <= 1){
-                progressBar.progressProperty().setValue(counter);
-                try {
-                    TimeUnit.MICROSECONDS.sleep(Math.round(0.0001 * (Double.valueOf(tempString)) * 1_000_000));
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                counter += 0.0001;
-            }
-
+            controller.startSimulation();
             //Output results
             //Average Build Time-----------------------------------------------------------------------------------------------------------------------------------------
             String output1FinalText = "Tempo médio que cada tipo de modelo demora a ser construido: \n\n";
 
             HashMap<String, Double> averageBuildTime = controller.getModelAverageBuildTime();
-            for(String key : averageBuildTime.keySet()){
-                output1FinalText += ("M"+ key + ": " + averageBuildTime.get(key) + " horas.\n");
+            for (String key : averageBuildTime.keySet()) {
+                output1FinalText += ("M" + key + ": " + averageBuildTime.get(key) + " horas.\n");
             }
 
             averageBuildTimeOutput.setText(output1FinalText);
@@ -187,7 +189,7 @@ public class GUI extends Application implements Initializable {
             String outputAverageWaitTimeOutput = "Tempo médio que cada tipo de modelo fica em espera: \n\n";
 
             HashMap<String, Double> averageWaitTime = controller.getAverageWaitTime();
-            for(String key : averageWaitTime.keySet()){
+            for (String key : averageWaitTime.keySet()) {
                 outputAverageWaitTimeOutput += ("M" + key + ": " + averageWaitTime.get(key) + " horas.\n");
             }
 
@@ -198,7 +200,7 @@ public class GUI extends Application implements Initializable {
             String outputAverageUsagePercentageOutput = "Percentagem de tempo de utilização de cada linha de trabalho: \n\n";
 
             HashMap<String, ArrayList<Double>> linesAverages = controller.getLinesAverages();
-            for(String key : linesAverages.keySet()) {
+            for (String key : linesAverages.keySet()) {
                 int line = 1;
                 for (Double value : linesAverages.get(key)) {
                     outputAverageUsagePercentageOutput += ("Z" + key + " L" + line + ": " + value + "%\n");
